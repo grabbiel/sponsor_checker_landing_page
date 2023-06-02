4da10159c9d8703dab72e1fa4fb3b1296dbb2bf2
pakitow <fcruzado@mail.stmarytx.edu>
2023-06-02 12:45:02 -0500

    added donation href

--- src/components/donate/Donate.tsx
+++ src/components/donate/Donate.tsx
@@ -2,10 +2,7 @@ function Donate()
 {
     return(
         <div className="current-page h-[100rem] w-full bg-[#00BD72] my-14">
-            <div>
-                <p>You are in Donate page</p>
-            </div>
-            <div></div>
+            <a href="https://donate.stripe.com/bIYg0A5EidqVa7C6op" target="_blank"><button>Donate anything to this project, if you feel like it!</button></a>
         </div>
     );
 }
